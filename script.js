1a92cc5db5d14996d954e20a3f4ce6933f89b062
Dmitriy <dima.ordynskiy@gmail.com>
2022-05-11 18:51:28 +0300

    div centered

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const likeBtn = document.querySelector(".btn-primary");
 const likesQuantity = document.querySelector(".like");
 
 likeBtn.addEventListener("click", () => {
-  likesQuantity.textContent = "1 лайк";
+  let likes = 0;
+  likes += 1;
+  const word = "лайк";
+  likesQuantity.innerHTML = `${likes} ${word}`;
 });
